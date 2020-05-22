08009978fb36e0a6ff43414098f206a9b258db5c
Subesh Bhandari <subeshb1@gmail.com>
2020-05-22 19:57:45 +0545

    Update root component

--- gatsby-site/gatsby-browser.js
+++ gatsby-site/gatsby-browser.js
@@ -2,18 +2,9 @@
 import 'typeface-lora'
 import 'typeface-libre-baskerville'
 import 'typeface-ibm-plex-mono'
-require('prismjs/plugins/command-line/prism-command-line.css')
 import { ThemeProvider, CSSReset } from '@chakra-ui/core'
 import customTheme from './theme'
 
-function App({ children }) {
-  return (
-    <ThemeProvider theme={customTheme}>
-      <CSSReset />
-      {children}
-    </ThemeProvider>
-  )
+export const wrapRootElement = ({ element }) => {
+  return <ThemeProvider theme={customTheme}>{element}</ThemeProvider>
 }
-// import { wrapRootElement as wrap } from './wrap-root-element'
-
-// export const wrapRootElement = wrap
