3aed95cc63f7bf034ae6c474fd616ca8ef076237
Subesh Bhandari <subeshb1@gmail.com>
2020-05-24 01:27:04 +0545

    Add theme picker

--- gatsby-site/src/components/navbar/ThemePicker.jsx
+++ gatsby-site/src/components/navbar/ThemePicker.jsx
@@ -48,67 +48,159 @@ const themes = [
     '--theme-secondary-color': '#ffffff',
     '--theme-secondary-bg': '#f4f4f4',
     '--theme-primary-hover': '#e0e0e096',
-    '--theme-navbar-box-shadow': '#131313ab',
+    '--theme-navbar-box-shadow': '#424653',
   },
 ]
 
+const variableMap = {
+  '--theme-primary-bg': 'Primary BG',
+  '--theme-primary-color': 'Primary Color',
+  '--theme-secondary-color': 'Secondary Color',
+  '--theme-secondary-bg': 'Secondary BG',
+  '--theme-primary-hover': 'Primary Hover',
+  '--theme-navbar-box-shadow': 'Navbar Shadow',
+}
+
+const primaryColors = [
+  {
+    name: 'Blue',
+    variables: {
+      '--theme-primary-bg': '#1d5baf',
+      '--theme-primary-color': '#000000',
+      '--theme-secondary-color': '#ffffff',
+      '--theme-secondary-bg': '#000000',
+      '--theme-primary-hover': '#3068b7',
+      '--theme-navbar-box-shadow': '#131313ab',
+    },
+  },
+  {
+    name: 'Red',
+    variables: {
+      '--theme-primary-bg': '#af1d4c',
+      '--theme-primary-color': '#000000',
+      '--theme-secondary-color': '#ffffff',
+      '--theme-secondary-bg': '#f4f4f4',
+      '--theme-primary-hover': '#e4316a96',
+      '--theme-navbar-box-shadow': '#131313ab',
+    },
+  },
+  {
+    name: 'White',
+    variables: {
+      '--theme-primary-bg': '#ffffff',
+      '--theme-primary-color': '#000000',
+      '--theme-secondary-color': '#000000',
+      '--theme-secondary-bg': '#f4f4f4',
+      '--theme-primary-hover': '#e0e0e096',
+      '--theme-navbar-box-shadow': '#c9c9c9ab',
+    },
+  },
+  {
+    name: 'Black',
+    variables: {
+      '--theme-primary-bg': '#2a2c35',
+      '--theme-primary-color': '#000000',
+      '--theme-secondary-color': '#ffffff',
+      '--theme-secondary-bg': '#f4f4f4',
+      '--theme-primary-hover': '#424653',
+      '--theme-navbar-box-shadow': '#131313ab',
+    },
+  },
+]
+
+const ThemeButton = ({ name, onClick, background }) => (
+  <button className="theme-picker__item" onClick={onClick}>
+    <div className="theme-picker__object" style={{ background }}></div>
+    {name}
+  </button>
+)
 function ThemeChanger() {
-  const [value, setValue, remove] = useLocalStorage('color-theme', {
-    '--theme-primary-bg': '#1d5baf',
-    '--theme-primary-color': '#000000',
-    '--theme-secondary-color': '#ffffff',
-    '--theme-secondary-bg': '#000000',
-    '--theme-primary-hover': '#3068b7',
-    '--theme-navbar-box-shadow': '#c9c9c9ab',
-  })
-  const [color, setColor] = useState('#4b3b9b')
-
-  const [change, setchange] = useState(0)
+  const [primaryColor, setPrimaryColor, remove] = useLocalStorage(
+    'color-theme',
+    {
+      '--theme-primary-bg': '#1d5baf',
+      '--theme-primary-color': '#000000',
+      '--theme-secondary-color': '#ffffff',
+      '--theme-secondary-bg': '#000000',
+      '--theme-primary-hover': '#3068b7',
+      '--theme-navbar-box-shadow': '#c9c9c9ab',
+    }
+  )
   useEffect(() => {
-    Object.entries(value).map(([key, value]) => {
+    Object.entries(primaryColor).map(([key, value]) => {
       document.documentElement.style.setProperty(key, value)
     })
-  }, [value])
-
-  useEffect(() => {
-    Object.entries(value).map(([key, value]) => {
-      document.documentElement.style.setProperty(key, value)
-    })
-  }, [value])
+  }, [primaryColor])
+  const [show, setShow] = useState(false)
   return (
     <div className="theme-picker">
-      <button
-        type="button"
-        onClick={() => {
-          const newTheme = (change + 1) % themes.length
-          setchange(newTheme)
-          setValue(themes[newTheme])
-        }}
-      >
-        Change theme
-      </button>
-      <Popover elementAs="input" value={color} readOnly>
-        <SketchPicker
-          color={color}
-          onChange={color => {
-            setColor(color.hex)
-          }}
-        />
-      </Popover>
+      <div className="theme-picker__theme">
+        <div className="theme-picker__header">Theme</div>
+        <div className="theme-picker__content">
+          {themes.map((x, i) => {
+            return (
+              <div className="theme-picker__item" key={i}>
+                <div className="theme-picker__object"></div>
+                {x.name}
+              </div>
+            )
+          })}
+        </div>
+        <div className="theme-picker__header">Primary Colors</div>
+        <div className="theme-picker__content">
+          {primaryColors.map((x, i) => {
+            return (
+              <ThemeButton
+                key={i}
+                onClick={() => setPrimaryColor(x.variables)}
+                background={x.variables['--theme-primary-bg']}
+                name={x.name}
+              />
+            )
+          })}
+        </div>
+        <button onClick={() => setShow(!show)}>{!show?"Show more":"Hide"}</button>
+        {show &&
+          Object.entries(primaryColor).map(([key, value], i) => {
+            return (
+              <div className="theme-picker__input-group">
+                <div className="theme-picker__header">{variableMap[key]}</div>
+                <Popover
+                  key={i}
+                  elementAs="input"
+                  value={value}
+                  readOnly
+                  placement="bottom"
+                >
+                  <SketchPicker
+                    color={value}
+                    onChange={color => {
+                      setPrimaryColor({
+                        ...primaryColor,
+                        [key]: color.hex,
+                      })
+                    }}
+                  />
+                </Popover>
+              </div>
+            )
+          })}
+      </div>
     </div>
   )
 }
 export default function ThemePicker() {
   return (
     <Popover
-      show
-      debug={"EWHATT"}
+      // show
+      debug={'EWHATT'}
       elementAs={React.forwardRef((props, ref) => (
         <Tooltip
+          elementAs="button"
           closeOnClick
           ref={ref}
           {...props}
-          placement="bottom"
+          placement="left"
           text={'Change website theme'}
           className="lg-navbar__item lg-navbar__item--right lg-navbar__item--circular"
         >
