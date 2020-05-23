f273110ea03a3f79b47759531b6843de7da372ff
Subesh Bhandari <subeshb1@gmail.com>
2020-05-23 21:39:25 +0545

    Refactor ThemePicker

--- gatsby-site/src/components/navbar/ThemePicker.jsx
+++ gatsby-site/src/components/navbar/ThemePicker.jsx
@@ -1,9 +1,81 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Popover from '../Popover'
 import Tooltip from '../ToolTip'
 import { IoIosColorPalette } from 'react-icons/io'
 import { SketchPicker } from 'react-color'
 import { useLocalStorage } from 'react-use'
+
+const darkBlue = {
+  bg: '#083575',
+  color: 'white',
+  'box-shadow': '#00193e',
+}
+
+const darkRed = {
+  bg: '#7a1522',
+  color: 'white',
+  'box-shadow': '#581019',
+}
+
+const themes = [
+  {
+    '--theme-primary-bg': '#1d5baf',
+    '--theme-primary-color': '#000000',
+    '--theme-secondary-color': '#ffffff',
+    '--theme-secondary-bg': '#000000',
+    '--theme-primary-hover': '#3068b7',
+    '--theme-navbar-box-shadow': '#131313ab',
+  },
+  {
+    '--theme-primary-bg': '#af1d4c',
+    '--theme-primary-color': '#000000',
+    '--theme-secondary-color': '#ffffff',
+    '--theme-secondary-bg': '#f4f4f4',
+    '--theme-primary-hover': '#e4316a96',
+    '--theme-navbar-box-shadow': '#131313ab',
+  },
+  {
+    '--theme-primary-bg': '#ffffff',
+    '--theme-primary-color': '#000000',
+    '--theme-secondary-color': '#000000',
+    '--theme-secondary-bg': '#f4f4f4',
+    '--theme-primary-hover': '#e0e0e096',
+    '--theme-navbar-box-shadow': '#c9c9c9ab',
+  },
+  {
+    '--theme-primary-bg': '#2a2c35',
+    '--theme-primary-color': '#000000',
+    '--theme-secondary-color': '#ffffff',
+    '--theme-secondary-bg': '#f4f4f4',
+    '--theme-primary-hover': '#e0e0e096',
+    '--theme-navbar-box-shadow': '#131313ab',
+  },
+]
+
+function ThemeChanger() {
+  return (
+    <div className="theme-picker">
+      <button
+        type="button"
+        onClick={() => {
+          const newTheme = (change + 1) % themes.length
+          setchange(newTheme)
+          setValue(themes[newTheme])
+        }}
+      >
+        Change theme
+      </button>
+      <Popover elementAs="input" value={color} readOnly>
+        <SketchPicker
+          color={color}
+          onChange={color => {
+            setColor(color.hex)
+          }}
+        />
+      </Popover>
+    </div>
+  )
+}
 export default function ThemePicker() {
   const [value, setValue, remove] = useLocalStorage('color-theme', {
     '--theme-primary-bg': '#1d5baf',
@@ -11,48 +83,18 @@ export default function ThemePicker() {
     '--theme-secondary-color': '#ffffff',
     '--theme-secondary-bg': '#000000',
     '--theme-primary-hover': '#3068b7',
-    '--theme-navbar-box-shadow': '#484848ab',
+    '--theme-navbar-box-shadow': '#c9c9c9ab',
   })
-  const [color, setColor] = useState(
-    '#4b3b9b'
-)
+  const [color, setColor] = useState('#4b3b9b')
+
+  useEffect(() => {
+    Object.entries(value).map(([key, value]) => {
+      document.documentElement.style.setProperty(key, value)
+    })
+  }, [value])
 
   const [change, setchange] = useState(0)
 
-  const themes = [
-    {
-      '--theme-primary-bg': '#1d5baf',
-      '--theme-primary-color': '#000000',
-      '--theme-secondary-color': '#ffffff',
-      '--theme-secondary-bg': '#000000',
-      '--theme-primary-hover': '#3068b7',
-      '--theme-navbar-box-shadow': '#484848ab',
-    },
-    {
-      '--theme-primary-bg': '#af1d4c',
-      '--theme-primary-color': '#000000',
-      '--theme-secondary-color': '#ffffff',
-      '--theme-secondary-bg': '#f4f4f4',
-      '--theme-primary-hover': '#e4316a96',
-      '--theme-navbar-box-shadow': '#838383ab',
-    },
-    {
-      '--theme-primary-bg': '#ffffff',
-      '--theme-primary-color': '#000000',
-      '--theme-secondary-color': '#000000',
-      '--theme-secondary-bg': '#f4f4f4',
-      '--theme-primary-hover': '#e0e0e096',
-      '--theme-navbar-box-shadow': '#796c6cab',
-    },
-    {
-      '--theme-primary-bg': '#000000',
-      '--theme-primary-color': '#000000',
-      '--theme-secondary-color': '#ffffff',
-      '--theme-secondary-bg': '#f4f4f4',
-      '--theme-primary-hover': '#e0e0e096',
-      '--theme-navbar-box-shadow': '#796c6cab',
-    },
-  ]
   return (
     <Popover
       elementAs={React.forwardRef((props, ref) => (
@@ -60,50 +102,13 @@ export default function ThemePicker() {
           closeOnClick
           ref={ref}
           {...props}
-          placement="right"
+          placement="bottom"
           text={'Change website theme'}
           className="lg-navbar__item lg-navbar__item--right lg-navbar__item--circular"
         >
           <IoIosColorPalette size={'1.5em'} />
         </Tooltip>
       ))}
-    >
-      <form>
-        <button
-        type="button"
-          onClick={() => {
-            const newTheme = (change + 1) % themes.length
-            console.log(value)
-            Object.entries(themes[newTheme]).map(([key, value]) => {
-              document.documentElement.style.setProperty(key, value)
-              console.log()
-            })
-            setchange(newTheme)
-            setValue({
-              '--theme-primary-bg': '#af1d4c',
-              '--theme-primary-color': '#000000',
-              '--theme-secondary-color': '#ffffff',
-              '--theme-secondary-bg': '#f4f4f4',
-              '--theme-primary-hover': '#e4316a96',
-              '--theme-navbar-box-shadow': '#838383ab',
-            })
-          }}
-        >
-          Change theme
-        </button>
-        <Popover elementAs="input" value={color} readOnly >
-          <SketchPicker
-            color={color}
-            onChange={color => {
-              setColor(color.hex)
-            }}
-          />
-        </Popover>
-      </form>
-    </Popover>
+    ></Popover>
   )
-}
-
-{
-  /* <SketchPicker /> */
 }
