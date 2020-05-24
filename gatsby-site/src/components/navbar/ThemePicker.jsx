d6680d5610a3a94f388f499312c8ff6ba7187b7b
Subesh Bhandari <subeshb1@gmail.com>
2020-05-24 12:38:07 +0545

    Support theme switch

--- gatsby-site/src/components/navbar/ThemePicker.jsx
+++ gatsby-site/src/components/navbar/ThemePicker.jsx
@@ -19,36 +19,24 @@ const darkRed = {
 
 const themes = [
   {
-    '--theme-primary-bg': '#1d5baf',
-    '--theme-primary-color': '#000000',
-    '--theme-secondary-color': '#ffffff',
-    '--theme-secondary-bg': '#000000',
-    '--theme-primary-hover': '#3068b7',
-    '--theme-navbar-box-shadow': '#131313ab',
+    name: 'Light',
+    value: 'theme-light',
+    color: '#ffffff',
   },
   {
-    '--theme-primary-bg': '#af1d4c',
-    '--theme-primary-color': '#000000',
-    '--theme-secondary-color': '#ffffff',
-    '--theme-secondary-bg': '#f4f4f4',
-    '--theme-primary-hover': '#e4316a96',
-    '--theme-navbar-box-shadow': '#131313ab',
+    name: 'Dark',
+    value: 'theme-dark',
+    color: '#2a2c35',
   },
   {
-    '--theme-primary-bg': '#ffffff',
-    '--theme-primary-color': '#000000',
-    '--theme-secondary-color': '#000000',
-    '--theme-secondary-bg': '#f4f4f4',
-    '--theme-primary-hover': '#e0e0e096',
-    '--theme-navbar-box-shadow': '#c9c9c9ab',
+    name: 'Dark Blue',
+    value: 'theme-dark-blue',
+    color: '#083575',
   },
   {
-    '--theme-primary-bg': '#2a2c35',
-    '--theme-primary-color': '#000000',
-    '--theme-secondary-color': '#ffffff',
-    '--theme-secondary-bg': '#f4f4f4',
-    '--theme-primary-hover': '#e0e0e096',
-    '--theme-navbar-box-shadow': '#424653',
+    name: 'Dark Red',
+    value: 'theme-dark-red',
+    color: '#7a1522',
   },
 ]
 
@@ -106,6 +94,28 @@ const primaryColors = [
       '--theme-navbar-box-shadow': '#131313ab',
     },
   },
+  {
+    name: 'Dark Blue',
+    variables: {
+      '--theme-primary-bg': '#083575',
+      '--theme-primary-color': '#000000',
+      '--theme-secondary-color': '#ffffff',
+      '--theme-secondary-bg': '#f4f4f4',
+      '--theme-primary-hover': '#424653',
+      '--theme-navbar-box-shadow': '#131313ab',
+    },
+  },
+  {
+    name: 'Dark Red',
+    variables: {
+      '--theme-primary-bg': '#7a1522',
+      '--theme-primary-color': '#000000',
+      '--theme-secondary-color': '#ffffff',
+      '--theme-secondary-bg': '#f4f4f4',
+      '--theme-primary-hover': '#424653',
+      '--theme-navbar-box-shadow': '#131313ab',
+    },
+  },
 ]
 
 const ThemeButton = ({ name, onClick, background }) => (
@@ -114,23 +124,7 @@ const ThemeButton = ({ name, onClick, background }) => (
     {name}
   </button>
 )
-function ThemeChanger() {
-  const [primaryColor, setPrimaryColor, remove] = useLocalStorage(
-    'color-theme',
-    {
-      '--theme-primary-bg': '#1d5baf',
-      '--theme-primary-color': '#000000',
-      '--theme-secondary-color': '#ffffff',
-      '--theme-secondary-bg': '#000000',
-      '--theme-primary-hover': '#3068b7',
-      '--theme-navbar-box-shadow': '#c9c9c9ab',
-    }
-  )
-  useEffect(() => {
-    Object.entries(primaryColor).map(([key, value]) => {
-      document.documentElement.style.setProperty(key, value)
-    })
-  }, [primaryColor])
+function ThemeChanger({ setPrimaryColor, primaryColor, setThemeMode }) {
   const [show, setShow] = useState(false)
   return (
     <div className="theme-picker">
@@ -139,10 +133,12 @@ function ThemeChanger() {
         <div className="theme-picker__content">
           {themes.map((x, i) => {
             return (
-              <div className="theme-picker__item" key={i}>
-                <div className="theme-picker__object"></div>
-                {x.name}
-              </div>
+              <ThemeButton
+                key={i}
+                onClick={() => setThemeMode(x.value)}
+                background={x.color}
+                name={x.name}
+              />
             )
           })}
         </div>
@@ -159,7 +155,9 @@ function ThemeChanger() {
             )
           })}
         </div>
-        <button onClick={() => setShow(!show)}>{!show?"Show more":"Hide"}</button>
+        <button onClick={() => setShow(!show)}>
+          {!show ? 'Show more' : 'Hide'}
+        </button>
         {show &&
           Object.entries(primaryColor).map(([key, value], i) => {
             return (
@@ -190,6 +188,33 @@ function ThemeChanger() {
   )
 }
 export default function ThemePicker() {
+  const [primaryColor, setPrimaryColor, remove] = useLocalStorage(
+    'color-theme',
+    {
+      '--theme-primary-bg': '#1d5baf',
+      '--theme-primary-color': '#000000',
+      '--theme-secondary-color': '#ffffff',
+      '--theme-secondary-bg': '#000000',
+      '--theme-primary-hover': '#3068b7',
+      '--theme-navbar-box-shadow': '#c9c9c9ab',
+    }
+  )
+
+  const [themeMode, setThemeMode, removeTheme] = useLocalStorage(
+    'theme',
+    'theme-light'
+  )
+
+  useEffect(() => {
+    Object.entries(primaryColor).map(([key, value]) => {
+      document.documentElement.style.setProperty(key, value)
+    })
+  }, [primaryColor])
+
+  useEffect(() => {
+    document.getElementById('project-main-container').className = themeMode
+  }, [themeMode])
+
   return (
     <Popover
       // show
@@ -208,7 +233,11 @@ export default function ThemePicker() {
         </Tooltip>
       ))}
     >
-      <ThemeChanger />
+      <ThemeChanger
+        primaryColor={primaryColor}
+        setPrimaryColor={setPrimaryColor}
+        setThemeMode={setThemeMode}
+      />
     </Popover>
   )
 }
