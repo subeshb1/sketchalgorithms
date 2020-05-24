f00baad252afe45cd84760c3fa0143836f0c3085
Subesh Bhandari <subeshb1@gmail.com>
2020-05-24 13:44:22 +0545

    Use strategy fixed for navbar popovers

--- gatsby-site/src/components/navbar/index.jsx
+++ gatsby-site/src/components/navbar/index.jsx
@@ -13,7 +13,7 @@ export default function NavBar() {
       <Link to="/" className="lg-navbar__item lg-navbar__header">
         Subesh Bhandari
       </Link>
-      <Popover elementAs={navDropDownFactory('/blogs', 'Blogs')}>
+      <Popover strategy="fixed" elementAs={navDropDownFactory('/blogs', 'Blogs')}>
         <div className="lg-navbar__drop-down" key="1">
           <div>1</div>
           <div>1</div>
@@ -23,14 +23,14 @@ export default function NavBar() {
           <div>3</div>
         </div>
       </Popover>
-      <Popover elementAs={navDropDownFactory('/apps', 'Apps')}>
+      <Popover strategy="fixed" elementAs={navDropDownFactory('/apps', 'Apps')}>
         <div className="lg-navbar__drop-down" key="1">
           <div>1</div>
           <div>2</div>
           <div>3</div>
         </div>
       </Popover>
-      <Popover elementAs={navDropDownFactory('/projects', 'Projects')}>
+      <Popover strategy="fixed" elementAs={navDropDownFactory('/projects', 'Projects')}>
         <div className="lg-navbar__drop-down" key="1">
           <div>1</div>
           <div>2</div>
