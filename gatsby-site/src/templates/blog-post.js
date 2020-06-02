7673ffd9b0bdc179d7f34c0d576eaf73300c82c1
Subesh Bhandari <subeshb1@gmail.com>
2020-06-02 23:06:51 +0545

    Update disqus and layout

--- gatsby-site/src/templates/blog-post.js
+++ gatsby-site/src/templates/blog-post.js
@@ -6,8 +6,7 @@ import Bio from '../components/Bio'
 import Layout from '../components/Layouts/Layout'
 import SEO from '../components/seo'
 import { rhythm, scale } from '../utils/typography'
-import Disqus from 'disqus-react'
-import BlogPostTemplate from '../components/Layouts/BlogLayout'
+import Disqus, { DiscussionEmbed } from 'disqus-react'
 import Toc from '../components/Toc'
 
 class BlogPost extends React.Component {
@@ -15,67 +14,74 @@ class BlogPost extends React.Component {
     const post = this.props.data.markdownRemark
     const siteTitle = this.props.data.site.siteMetadata.title
     const { previous, next } = this.props.pageContext
-    const disqusShortname = 'sketchalgorithm'
-    console.log(post)
+    const disqusShortname = 'subeshbhandari'
+    console.log(post) 
     const disqusConfig = {
       url: 'https://www.subeshbhandari.com' + post.fields.slug,
       identifier: post.fields.slug,
       title: siteTitle,
     }
     return (
-      <BlogPostTemplate>
+      <Layout>
         <SEO title={post.frontmatter.title} description={post.excerpt} />
-        <h1>{post.frontmatter.title}</h1>
-        <Toc tableOfContents={post.tableOfContents} />
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: `block`,
-            marginBottom: rhythm(1),
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: post.html }}
-        />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
-        <Bio />
-
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-        <Disqus.DiscussionEmbed
-          shortname={disqusShortname}
-          config={disqusConfig}
-        />
-      </BlogPostTemplate>
+        <div className="blog-main-container">
+          <div className="blog-left-container">
+            
+          </div>
+          <div className="blog-mid-container">
+            <h1>{post.frontmatter.title}</h1>
+            <Toc tableOfContents={post.tableOfContents} />
+            <p
+              style={{
+                ...scale(-1 / 5),
+                display: `block`,
+                marginBottom: rhythm(1),
+              }}
+            >
+              {post.frontmatter.date}
+            </p>
+            <div
+              className="blog-post-content"
+              dangerouslySetInnerHTML={{ __html: post.html }}
+            />
+            <hr
+              style={{
+                marginBottom: rhythm(1),
+              }}
+            />
+            <Bio />
+            <ul
+              style={{
+                display: `flex`,
+                flexWrap: `wrap`,
+                justifyContent: `space-between`,
+                listStyle: `none`,
+                padding: 0,
+              }}
+            >
+              <li>
+                {previous && (
+                  <Link to={previous.fields.slug} rel="prev">
+                    ← {previous.frontmatter.title}
+                  </Link>
+                )}
+              </li>
+              <li>
+                {next && (
+                  <Link to={next.fields.slug} rel="next">
+                    {next.frontmatter.title} →
+                  </Link>
+                )}
+              </li>
+            </ul>
+            <Disqus.DiscussionEmbed
+              shortname={disqusShortname}
+              config={disqusConfig}
+            />
+          </div>
+          
+        </div>
+      </Layout>
     )
   }
 }
