5370656532e52e3265d071315b2e2d26df172c12
Subesh Bhandari <subeshb1@gmail.com>
2020-06-04 17:58:09 +0545

    Remove failure codes

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@ cd gatsby-site
 npm i && npm run build
 cd ../cra-site
 npm i && npm run build
-rm -rf ../gatsby-site/public/cra
 mv build ../gatsby-site/public/cra
 cd ../gatsby-site
 # netlify deploy --dir=public --prod
