73364345f618b2ece5285a328e314badc773c099
Subesh Bhandari <subeshb1@gmail.com>
2020-06-04 17:48:23 +0545

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd gatsby-site
-yarn build
+npm i && npm run build
 cd ../cra-site
-yarn build
+npm i && npm run build
 rm -rf ../gatsby-site/public/cra
 mv build ../gatsby-site/public/cra
 cd ../gatsby-site
+# netlify deploy --dir=public --prod
 cp ../_redirects public/
-netlify deploy --dir=public --prod
