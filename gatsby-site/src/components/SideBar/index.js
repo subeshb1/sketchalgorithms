fae009ec7416bf5724819b9e568d80d972d0d32a
Subesh Bhandari <subeshb1@gmail.com>
2020-06-06 18:19:29 +0545

    Add sidebar links

--- gatsby-site/src/components/SideBar/index.js
+++ gatsby-site/src/components/SideBar/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { title } from 'change-case'
+import { Link } from 'gatsby'
 
 export default function SideBar({ seriesElements }) {
   return seriesElements && seriesElements.length ? (
@@ -7,7 +7,7 @@ export default function SideBar({ seriesElements }) {
       {seriesElements.map(element => {
         return (
           <div className="blog-sidebar__item" key={element.title}>
-            {element.title}
+            <Link to={element.slug}>{element.title}</Link>
           </div>
         )
       })}
