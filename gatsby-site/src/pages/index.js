c670eeb0e46e36a41a92ccea7903967ebaed3bf8
Subesh Bhandari <subeshb1@gmail.com>
2020-06-07 00:40:50 +0545

    Dont show docs in blogs and hide drafts

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -49,7 +49,10 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { timeToRead: {}, frontmatter: { type: { ne: "doc" }, draft: { ne : true } } }
+    ) {
       edges {
         node {
           excerpt
