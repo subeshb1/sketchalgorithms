e007e83901e38bb80861ff202e871fd92c27af0e
Subesh Bhandari <subeshb1@gmail.com>
2020-06-07 13:38:54 +0545

    Remove chakra wrapper

--- gatsby-site/gatsby-browser.js
+++ gatsby-site/gatsby-browser.js
@@ -2,9 +2,5 @@
 import 'typeface-lora'
 import 'typeface-libre-baskerville'
 import 'typeface-ibm-plex-mono'
-import { ThemeProvider, CSSReset } from '@chakra-ui/core'
-import customTheme from './theme'
 
-export const wrapRootElement = ({ element }) => {
-  return <ThemeProvider theme={customTheme}>{element}</ThemeProvider>
-}
+document.body && (document.body.className="theme-light")
