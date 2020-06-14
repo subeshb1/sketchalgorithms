97fd30713485f1d2c2fecf3a1f7b0edcf912c613
Subesh Bhandari <subeshb1@gmail.com>
2020-06-14 22:32:10 +0545

    Fix path in drawable

--- src/app/drawable-graph/drawable.container.js
+++ src/app/drawable-graph/drawable.container.js
@@ -13,34 +13,34 @@ const headData = {
       'Draw and Visualize Graph Algorithms. Breadth First Search (BFS) algorithm traverses a graph in a breadthward motion and uses a queue to remember to get the next vertex to start a search, when a dead end occurs in any iteration. Learn and visualize BFS',
     title: 'Breadth First Search (BFS) | Graph Algorithms | Subesh Bhandari',
     image: 'bfs-draw.png',
-    url: '/app/drawable/bfs',
+    url: '/app/drawable-graph/bfs',
   },
   dfs: {
     description:
       'Draw and Visualize Graph Algorithms. Depth-first search (DFS) is an algorithm for traversing or searching tree or graph data structures. The algorithm starts at the root node (selecting some arbitrary node as the root node in the case of a graph) and explores as far as possible along each branch before backtracking. Learn Visualize DFS',
     title: 'Depth First Search (DFS) | Graph Algorithms | Subesh Bhandari',
     image: 'dfs-draw.png',
-    url: '/app/drawable/dfs',
+    url: '/app/drawable-graph/dfs',
   },
   'a-star': {
     description:
       "Draw and Visualize Graph Algorithms. A* is the most popular choice for pathfinding, because it's fairly flexible and can be used in a wide range of contexts. A* is like Dijkstra's Algorithm in that it can be used to find a shortest path. Learn and Visualize A star.",
     title: 'A Star (A*) | Graph Algorithms | Subesh Bhandari',
     image: 'a-star-draw.png',
-    url: '/app/drawable/a-star',
+    url: '/app/drawable-graph/a-star',
   },
   dijkstras: {
     description:
       "Draw and Visualize Graph Algorithms. One algorithm for finding the shortest path from a starting node to a target node in a weighted graph is Dijkstra's algorithm.  Learn and Visualize Dijkstras",
     title: 'Dijkstras Sort | Graph Algorithms | Subesh Bhandari',
     image: 'dijkstras-draw.png',
-    url: '/app/drawable/dijkstras',
+    url: '/app/drawable-graph/dijkstras',
   },
   home: {
     description:
       'Draw and Visualize Graph Algorithms. Learn about different types of Graph Algorithms following different paradigms. Calculate Complexity, visualize, get programming code and implement it yourself. a star search, dijkstras search, dfs search,bfs search...',
     title: 'Graph Algorithms | Subesh Bhandari',
-    url: '/app/drawable/',
+    url: '/app/drawable-graph/',
   },
 }
 
