570ee798d05c373c873c5b54dfacd0018ab0b0fb
Subesh Bhandari <subeshb1@gmail.com>
2020-06-14 22:49:24 +0545

    Show all apps in app home

--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import Layout from '../../components/Layouts/Layout'
-
-
-
-const MainApp = React.memo(() => {
-  return <Layout>App path</Layout>
+import AppDisplayLayout from '../../components/Layouts/AppDisplayLayout'
+const MainApp = React.memo(({ data: { allApp } }) => {
+  return (
+    <Layout>
+      <AppDisplayLayout path="/" data={allApp} />
+    </Layout>
+  )
 })
 export default MainApp
+
+export const pageQuery = graphql`
+  query {
+    allApp {
+      nodes {
+        title
+        url
+        description
+        category
+        name
+      }
+    }
+  }
+`
