ee38c9c1fbfea118fd7aa3f780826fa0d69b79c1
Subesh Bhandari <subeshb1@gmail.com>
2020-06-14 22:50:01 +0545

    Show all graph in bashe

--- src/pages/app/graph-search/index.js
+++ src/pages/app/graph-search/index.js
@@ -1,18 +1,40 @@
 import React from 'react'
 import { Router } from '@reach/router'
 import Layout from '../../../components/Layouts/Layout'
-
+import AppDisplayLayout from '../../../components/Layouts/AppDisplayLayout'
 import { Graph } from '../../../app/containers'
-const MainApp = React.memo(() => {
-  const ssr = typeof window === "undefined"
+import { graphql } from 'gatsby'
+const MainApp = React.memo(({ data: { graphSearch } }) => {
+  const ssr = typeof window === 'undefined'
   return (
     <Layout>
-      {!ssr && (
+      {!ssr ? (
         <Router basepath="/app/graph-search">
+          <AppDisplayLayout
+            path="/"
+            data={graphSearch}
+            category="graph-search"
+          />
           <Graph path="/*" />
         </Router>
+      ) : (
+        'Loading...'
       )}
     </Layout>
   )
 })
 export default MainApp
+
+export const pageQuery = graphql`
+  query {
+    graphSearch: allApp(filter: { category: { eq: "graph-search" } }) {
+      nodes {
+        title
+        url
+        description
+        category
+        name
+      }
+    }
+  }
+`
