24ed3b883bd46630c3f3d486abd5c163eef49c28
Subesh Bhandari <subeshb1@gmail.com>
2020-06-14 18:17:27 +0545

    Add 404 styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,24 @@
 import React from 'react'
-
 import Layout from '../components/Layouts/Layout'
 import SEO from '../components/seo'
-
+import notFound from '../assets/svg/404.svg'
+import '../css/404.scss'
+import { Link } from 'gatsby'
 class NotFoundPage extends React.Component {
   render() {
     return (
       <Layout location={this.props.location}>
         <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <div className="not-found">
+          <img src={notFound} alt="404 not found" />
+          <div className="not-found__message">
+            <h1>Not Found</h1>
+            Looks like the page you are looking for couldn't be found.
+            <Link to="/" className="not-found__button">
+              Return Home
+            </Link>
+          </div>
+        </div>
       </Layout>
     )
   }
