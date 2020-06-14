3c3ba988360d22c41a8e622dbcff41e8a70eec1b
Subesh Bhandari <subeshb1@gmail.com>
2020-06-14 22:49:34 +0545

    Show all drawable in drawable

--- src/pages/app/drawable-graph/index.js
+++ src/pages/app/drawable-graph/index.js
@@ -2,16 +2,38 @@ import React from 'react'
 import { Router } from '@reach/router'
 import Layout from '../../../components/Layouts/Layout'
 import { DrawableGraph } from '../../../app/containers'
-const MainApp = React.memo(() => {
-  const ssr = typeof window === "undefined"
+import AppDisplayLayout from '../../../components/Layouts/AppDisplayLayout'
+const MainApp = React.memo(({ data: { sortingAlgo } }) => {
+  const ssr = typeof window === 'undefined'
   return (
     <Layout>
-      {!ssr && (
+      {!ssr ? (
         <Router basepath="/app/drawable-graph">
+          <AppDisplayLayout
+            path="/"
+            data={sortingAlgo}
+            category="drawable-graph"
+          />
           <DrawableGraph path="/*" />
         </Router>
+      ) : (
+        'Loading...'
       )}
     </Layout>
   )
 })
 export default MainApp
+
+export const pageQuery = graphql`
+  query {
+    sortingAlgo: allApp(filter: { category: { eq: "drawable-graph" } }) {
+      nodes {
+        title
+        url
+        description
+        category
+        name
+      }
+    }
+  }
+`
