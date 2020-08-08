31c36bf36bd6934351f5bea879b6235fcde15638
Subesh Bhandari <subeshb1@gmail.com>
2020-08-08 09:20:15 +0545

    use prop image instead of query

--- src/components/navbar/MobileNav.jsx
+++ src/components/navbar/MobileNav.jsx
@@ -22,63 +22,10 @@ const MobileNav = React.memo(props => {
     searching,
     sorting,
     api,
+    imageToAscii,
+    nepaliDate,
     snakeGame,
-  } = useStaticQuery(
-    graphql`
-      query imagesAndImages {
-        navLogo: file(absolutePath: { regex: "/logo.png/" }) {
-          childImageSharp {
-            fixed(width: 50, height: 50) {
-              ...GatsbyImageSharpFixed
-            }
-          }
-        }
-
-        snakeGrid: file(absolutePath: { regex: "/snake-grid.png/" }) {
-          childImageSharp {
-            fixed(width: 50, height: 50) {
-              ...GatsbyImageSharpFixed
-            }
-          }
-        }
-        searching: file(absolutePath: { regex: "/searching.png/" }) {
-          childImageSharp {
-            fixed(width: 50, height: 50) {
-              ...GatsbyImageSharpFixed
-            }
-          }
-        }
-        sorting: file(absolutePath: { regex: "/sorting.png/" }) {
-          childImageSharp {
-            fixed(width: 50, height: 50) {
-              ...GatsbyImageSharpFixed
-            }
-          }
-        }
-        toc: file(absolutePath: { regex: "/toc.png/i" }) {
-          childImageSharp {
-            fixed(width: 50, height: 50) {
-              ...GatsbyImageSharpFixed
-            }
-          }
-        }
-        api: file(absolutePath: { regex: "/api-test.png/" }) {
-          childImageSharp {
-            fixed(width: 50, height: 50) {
-              ...GatsbyImageSharpFixed
-            }
-          }
-        }
-        snakeGame: file(absolutePath: { regex: "/snake-game.png/" }) {
-          childImageSharp {
-            fixed(width: 50, height: 50) {
-              ...GatsbyImageSharpFixed
-            }
-          }
-        }
-      }
-    `
-  )
+  } = props
   return (
     <nav className="st-navbar">
       <Popover
