62da76813ae3dfb2957e595f640bf939e59b1be7
Subesh Bhandari <subeshb1@gmail.com>
2020-08-08 09:26:53 +0545

    Add seo to image to ascii

--- src/pages/app/wasm/image-to-ascii.js
+++ src/pages/app/wasm/image-to-ascii.js
@@ -1,13 +1,19 @@
 
-import React, {  } from 'react'
+import React, { } from 'react'
 import Layout from '../../../components/Layouts/Layout'
 import '../../../utils/wasm-go.js'
 import ImageToAscii from '../../../app/wasm/ImageToAscii';
 
+import SEO from '../../../components/seo'
 
 export default function ImageToAsciiPage() {
   return (
     <Layout>
+      <SEO
+        title="Convert image to text character"
+        keywords={[`image to ascii`, `image`, `ascii`, `ansi`, 'convert', 'convert image to ascii']}
+        description={"Convert image to ascii on the browser. This tool allows image to be converted in to text without in file upload to the server. It uses WebAssembly to run native code on the browser to convert image to ascii characters."}
+      />
       <ImageToAscii />
     </Layout>
   )
