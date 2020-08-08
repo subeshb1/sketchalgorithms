a5ecd6a57504645d3a09503951559023c319f947
Subesh Bhandari <subeshb1@gmail.com>
2020-08-08 09:23:21 +0545

    Seperate drop zone

--- src/pages/app/wasm/image-to-ascii.js
+++ src/pages/app/wasm/image-to-ascii.js
@@ -7,33 +7,10 @@ import AnsiUp from 'ansi_up';
 import ImageToAscii from '../../../app/wasm/ImageToAscii';
 
 
-function Basic(props) {
-  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
-
-  const files = acceptedFiles.map(file => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
-    </li>
-  ));
-
-  return (
-    <section className="container">
-      <div {...getRootProps({ className: 'dropzone' })}>
-        <input id="file"{...getInputProps()} />
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      </div>
-      <aside>
-        <h4>Files</h4>
-        <ul>{files}</ul>
-      </aside>
-    </section>
-  );
-}
-
 export default function ImageToAsciiPage() {
   return (
     <Layout>
-     <ImageToAscii/>
+      <ImageToAscii />
     </Layout>
   )
 }
